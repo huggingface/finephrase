4278f31d7b89bf77dade6f2be69d8b2a8007ddc7
Joel Niklaus <joel@hf.co>
2025-09-13 11:33:16 +0000

    added another faulty node

--- train_model.py
+++ train_model.py
@@ -340,7 +340,7 @@ def main():
 #SBATCH --begin=now+0minutes
 #SBATCH --time={args.time}
 #SBATCH --exclusive
-#SBATCH --exclude=ip-26-0-160-103,ip-26-0-160-242,ip-26-0-161-138,ip-26-0-161-178,ip-26-0-162-46
+#SBATCH --exclude=ip-26-0-160-103,ip-26-0-160-242,ip-26-0-161-138,ip-26-0-161-178,ip-26-0-162-46,ip-26-0-162-180
 {"#SBATCH --dependency=afterok:" + args.d if args.d else ""}
 {"#SBATCH --reservation=" + args.reservation if args.reservation else ""}
 
